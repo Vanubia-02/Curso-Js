1054958c0d0a67c33366168e48acbe4175bc25cb
Vanubia-02 <vanubia.if@gmil.com>
2023-10-22 12:54:38 -0300

    aula 7 - tipo de dado string

---
+++ aula 6/java script/js.js
@@ -0,0 +1,45 @@
+//------------aula 6 -------
+
+
+var numero= 1;
+var float= 5.2;
+
+//typeof serve para ver o tipo da variavel
+console.log(typeof numero);
+console.log(typeof float);
+
+var textComNumero= "1234";
+var SoNumero= 1234;
+
+console.log(textComNumero);
+console.log(SoNumero);
+var juntando=textComNumero+SoNumero;
+console.log(juntando);
+
+console.log(typeof textComNumero);
+console.log(typeof SoNumero);
+
+console.log(textComNumero+textComNumero);
+console.log(SoNumero+SoNumero);
+
+//------------aula 7 -------
+
+var nome= "vanubia"
+console.log(nome);
+console.log(nome+nome);
+console.log(typeof nome);
+console.log(typeof "nome");
+console.log(typeof "50000765");
+console.log(nome+ " é o meu nome");
+var frase = " é o meu nome";
+console.log(nome + frase);
+
+//função basica para aparcer mensagem no html usando java script
+
+document.write(nome + frase + "e essa é uma frase feita com java script com a funcção de document.write(nome + frase + )");
+
+console.log('ola "mundo"');
+
+
+
+
