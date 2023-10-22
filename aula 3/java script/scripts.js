09ca64d36629b6805af2dc5935032afd0c33d9e7
Vanubia-02 <vanubia.if@gmil.com>
2023-10-22 11:29:46 -0300

    aula 3

---
+++ aula 3/java script/scripts.js
@@ -0,0 +1,23 @@
+console.log("primeiro")
+
+console.log("ultimo")
+
+var a=1;
+
+console.log(a);
+
+a="teste";
+
+console.log(a);
+
+//codigo de uma linha 
+
+/* codigo de varias linhas 
+
+tomar cuidado com isso
+o arquivo js é mostrado no 
+inspecionar da
+pagina. então evitar
+ comentarios com dados sensiveis. 
+*/
+
